78c56b1f39cb6e6fe91e95d9999cda643128b336
WO <wo@kotocoin.info>
2018-06-08 21:57:10 +0900

    Not yet Activation

--- src/chainparams.cpp
+++ src/chainparams.cpp
@@ -103,7 +103,7 @@ public:
         consensus.vUpgrades[Consensus::UPGRADE_TESTDUMMY].nActivationHeight =
             Consensus::NetworkUpgrade::NO_ACTIVATION_HEIGHT;
         consensus.vUpgrades[Consensus::UPGRADE_OVERWINTER].nProtocolVersion = 170005;
-        consensus.vUpgrades[Consensus::UPGRADE_OVERWINTER].nActivationHeight = 283000;
+        consensus.vUpgrades[Consensus::UPGRADE_OVERWINTER].nActivationHeight = Consensus::NetworkUpgrade::NO_ACTIVATION_HEIGHT;
 
         /**
          * The message start string should be awesome! ⓩ❤
